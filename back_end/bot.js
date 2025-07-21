bd7c7e39535c3c2b00b88ee096e1f8d18926da14
Yonatan-Ethiopia <yonatanethiopia1@gmail.com>
2025-07-21 15:57:54 +0300

    updated server.js and bot.js

--- back_end/bot.js
+++ back_end/bot.js
@@ -1,25 +1,17 @@
-const express = require('express')
-const telegramBot=require('node-telegram-bot-api')
-const data = require('./models/dataModel')
-const addImage = require('.controllers/bot/botController');
+const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-const token=process.env.BOT_TOKEN;
-const port = process.env.PORT
-const app = express()
-const WEBHOOK_URL = process.env.WEBHOOK_URL
-const bot = new telegramBot( token, { webHook :{ port: port}})
-bot.setWebHook(`${WEBHOOK_URL}/bot${token}`)
-app.use(express.json())
-app.post(`/bot${token}`,(req,res)=>{
-	 bot.processUpdate(req.body)
-	 res.sendStatus(200)
-})
-bot.onStart( '/start', (msg)=>{
-	bot.sendMesssage(msg.chat.id," Welcome to AASTU GALLERY!/n You can use our mini app to watch the gallery!")
-}) 
-bot.on('message', (msg)=>{
-	addImage(bot,msg)
-})
-app.listen(port, ()=>{
-	console.log(`Server running on port ${port}`)
-});                      
+const addImage = require('./controllers/bot/botController');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token);
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome to AASTU GALLERY!");
+});
+
+bot.on('message', (msg) => {
+  addImage(bot, msg);
+});
+
+module.exports = bot;
+  
--- back_end/server.js
+++ back_end/server.js
@@ -1,13 +1,41 @@
-const express = require('express')
-const bot = require('/bot.js')
-const db = require('./config/db')
-require('dotenv').config()
-const port = process.env.PORT
-const app = express()
-const getRoute = require('/routes/getRoute')
-app.use(express.json())
-app.use('/api', getRoute)
-app.listen( port, async ()=>{
-	console.log(`Server running on port ${port}`)
-	await bot.setWehook('')
-})
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+const getRoute = require('./routes/getRoute');
+const addImage = require('./controllers/bot/botController');
+
+const app = express();
+app.use(express.json());
+
+// MongoDB connection
+require('./config/db')(); // assuming db.js exports a connect function
+
+// API routes
+app.use('/api', getRoute);
+
+// Telegram Bot setup
+const token = process.env.BOT_TOKEN;
+const port = process.env.PORT || 3000;
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+
+const bot = new TelegramBot(token, { webHook: { port: port } });
+bot.setWebHook(`${WEBHOOK_URL}/bot${token}`);
+
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome to AASTU GALLERY!\nUse our mini app to view the gallery!");
+});
+
+bot.on('message', (msg) => {
+  addImage(bot, msg);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
